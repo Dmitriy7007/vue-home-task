d5a9b91a2b097464d78f185e8a20e01780d54bf8
Dmitriy7007 <charhan86@gmail.com>
2022-08-28 00:10:19 +0700

    home-task/module_3

---
+++ src/data/products.js
@@ -0,0 +1,62 @@
+export default [
+  {
+    title: 'Стул обычный',
+    price: 3690,
+    image: '/img/stul-alex.webp',
+  },
+  {
+    title: 'Стул красивый',
+    price: 5660,
+    image: '/img/stul-aul.webp',
+  },
+  {
+    title: 'Стул для дачи',
+    price: 21790,
+    image: '/img/stul-chamberi-seryj.webp',
+  },
+  {
+    title: 'Стул как стул',
+    price: 24690,
+    image: '/img/stul-dzhulian.webp',
+  },
+  {
+    title: 'Стул чемпиона',
+    price: 14960,
+    image: '/img/stul-ehdvin.webp',
+  },
+  {
+    title: 'Стул посидеть',
+    price: 8960,
+    image: '/img/stul-kiwi-gold-korichnevyj.webp',
+  },
+  {
+    title: 'Стул Толика',
+    price: 39900,
+    image: '/img/stul-palma-gold-sinij.webp',
+  },
+  {
+    title: 'Стул Великий',
+    price: 34900,
+    image: '/img/stul-tochtonado.webp',
+  },
+  {
+    title: 'Стул для отдыха',
+    price: 4900,
+    image: '/img/stul-tolix-oranzhevyy.webp',
+  },
+  {
+    title: 'Стул PRO',
+    price: 6990,
+    image: '/img/stul-tolix-soft.webp',
+  },
+  {
+    title: 'Стул со скидкой',
+    price: 16560,
+    image: '/img/stul-tolix-zelenyy.webp',
+  },
+  {
+    title: 'Стул Вау',
+    price: 30690,
+    image: '/img/stul-vany-ogon.webp',
+  },
+];
